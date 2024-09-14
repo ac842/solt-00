abbe2c05c03c1725fde3b5674eb06ed1a4022795
ac842 <130188187+ac842@users.noreply.github.com>
2024-09-14 20:04:34 +0800

    Initial commit

---
+++ src/components/game/Reels/styles.module.scss.d.ts
@@ -0,0 +1,12 @@
+export type Styles = {
+  'bonus': string;
+  'bonus-small': string;
+  'reels': string;
+  'reels__bonus': string;
+};
+
+export type ClassNames = keyof Styles;
+
+declare const styles: Styles;
+
+export default styles;
